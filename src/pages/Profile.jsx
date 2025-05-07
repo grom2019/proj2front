668c5738f502d844ead1b37c2aec2daa8720008f
John <johnDou@mail.com>
2025-05-07 20:21:37 +0300

    профіль удосконалення1

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../../src/styles/Profile.css';
 
 function Profile() {
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   const token = localStorage.getItem('token');
 
   useEffect(() => {
-    if (!token) window.location.href = '/login';
+    if (!token) return window.location.href = '/login';
 
     const fetchUserData = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/auth/profile`, {
-          headers: { Authorization: `Bearer ${token}` },
+        const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/auth/profile`, {
+          headers: { Authorization: `Bearer ${token}` }
         });
-        setUserData(response.data);
+        setUserData(res.data);
         setLoading(false);
       } catch {
         setError('Error fetching profile data');
@@ -30,12 +30,33 @@ function Profile() {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
+  const {
+    username, email, first_name, last_name, middle_name,
+    birth_date, military_unit, rank, position, vos, profile_image
+  } = userData;
+
   return (
     <div className="profile-container">
-      <h2>Profile</h2>
-      <p><strong>Username:</strong> {userData.username}</p>
-      <p><strong>Email:</strong> {userData.email}</p>
-      <p><strong>Password:</strong> *****</p>
+      {profile_image && (
+        <div className="avatar-container">
+          <img src={profile_image} alt="Profile" className="avatar" />
+        </div>
+      )}
+      <h2>Профіль</h2>
+      <p><strong>Логін:</strong> {username}</p>
+      <p><strong>Email:</strong> {email}</p>
+      <p><strong>Ім'я:</strong> {first_name}</p>
+      <p><strong>Прізвище:</strong> {last_name}</p>
+      <p><strong>По батькові:</strong> {middle_name}</p>
+      <p><strong>Дата народження:</strong> {birth_date}</p>
+      <p><strong>Військова частина:</strong> {military_unit}</p>
+      {military_unit && (
+        <>
+          <p><strong>Звання:</strong> {rank}</p>
+          <p><strong>Посада:</strong> {position}</p>
+          <p><strong>ВОС:</strong> {vos}</p>
+        </>
+      )}
     </div>
   );
 }
