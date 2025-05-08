c3245c4c00f8548a198451167e35920021b56c26
John <johnDou@mail.com>
2025-05-08 20:07:39 +0300

    правка навбар

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,17 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import LogoutButton from './LogoutButton';
+import '../styles/Navbar.css'; // Імпорт стилів
 
 const Navbar = () => {
   const { token } = useContext(AuthContext);
 
   return (
-    <header className="nav">
-      <div className="nav-container">
+    <header className="navbar">
+      <div className="navbar-left">
         {token ? (
           <>
             <Link to="/home" className="nav-link">Головна</Link>
-            <Link to="/profile" className="nav-link">Профіль</Link>
             <LogoutButton />
           </>
         ) : (
@@ -22,6 +22,9 @@ const Navbar = () => {
             <Link to="/register" className="nav-link">Реєстрація</Link>
           </>
         )}
+      </div>
+      <div className="navbar-right">
+        {token && <Link to="/profile" className="nav-link">Профіль</Link>}
       </div>
     </header>
   );
